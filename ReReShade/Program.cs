a9ff8f8437b94792ba621c431ccf47a91f6ac74a
eqbot <trentkenn8@gmail.com>
2023-02-06 17:45:55 -0800

    change process to include GShade uninstall

--- ReReShade/Program.cs
+++ ReReShade/Program.cs
@@ -1,6 +1,7 @@
 ﻿using IniParser;
+using System.Diagnostics;
 
-Console.Write("Enter the path to your FFXIV installation");
+Console.Write("Enter the path to your FFXIV installation: ");
 string? installPath = Console.ReadLine();
 
 if(installPath == null)
@@ -58,10 +59,11 @@ if(Directory.Exists(Path.Combine(installPath, "gshade-shaders")))
 
 //read GShade ini to find the shaders
 var iniParser = new FileIniDataParser();
+
 var data = iniParser.ReadFile(Path.Combine(installPath, "GShade.ini"));
 var GShadeEffectSearchPathsString = data["GENERAL"]["EffectSearchPaths"];
 var GShadeEffectSearchPaths = GShadeEffectSearchPathsString.Split(',');
-string? GShadeShadersPath = null, GShadeComputeShadersPath = null;
+string? GShadeShadersPath = null, GShadeComputeShadersPath = null, GShadeTexturesPath = null;
 foreach(var path in GShadeEffectSearchPaths)
 {
     if(Path.GetFileName(path) == "**")
@@ -83,9 +85,29 @@ if(GShadeShadersPath == null || GShadeComputeShadersPath == null)
     Console.WriteLine("Unable to read GShade shaders location from INI. They may have changed the format or some other issue may have occurred.");
     return;
 }
+var GShadeTextureSearchPathsString = data["GENERAL"]["TextureSearchPaths"];
+var GShadeTextureSearchPaths = GShadeTextureSearchPathsString.Split(',');
+foreach (var path in GShadeTextureSearchPaths)
+{
+    if (Path.GetFileName(path) == "**")
+    {
+        //this is a custom shaders path, do nothing
+        continue;
+    }
+    if (Path.GetFileName(path) == "Textures")
+    {
+        GShadeTexturesPath = path;
+    }
+}
+if (GShadeTexturesPath == null)
+{
+    Console.WriteLine("Unable to read GShade textures location from INI. They may have changed the format or some other issue may have occurred.");
+    return;
+}
 
-//unhook GShade and copy shaders
-var newShadersFolder = Path.Combine(installPath, "gshade-shaders");
+
+//copy shaders somewhere safe for gshade uninstall
+var newShadersFolder = Path.Combine(Path.GetTempPath(), "temp-gshade-shaders");
 Directory.CreateDirectory(newShadersFolder);
 Directory.CreateDirectory(Path.Combine(newShadersFolder, "Shaders"));
 foreach(var file in Directory.EnumerateFiles(GShadeShadersPath))
@@ -97,10 +119,57 @@ foreach (var file in Directory.EnumerateFiles(GShadeComputeShadersPath))
 {
     File.Copy(file, Path.Combine(newShadersFolder, "ComputeShaders", Path.GetFileName(file)));
 }
-if(File.Exists(Path.Combine(installPath, "dxgi.dll")))
+Directory.CreateDirectory(Path.Combine(newShadersFolder, "Textures"));
+foreach (var file in Directory.EnumerateFiles(GShadeTexturesPath))
 {
-    File.Delete(Path.Combine(installPath, "dxgi.dll"));
+    File.Copy(file, Path.Combine(newShadersFolder, "Textures", Path.GetFileName(file)));
 }
+Directory.Move(Path.Combine(installPath, "gshade-presets"), Path.Combine(installPath, "gshade-presets-backup"));
+
+//uninstall gshade
+var uninstallerPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "GShade", "GShade Uninstaller.exe");
+if (File.Exists(uninstallerPath))
+{
+    //our uninstaller exists, run it
+    Console.WriteLine("Launched GShade uninstaller (DO NOT RESTART)");
+    var process = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = uninstallerPath,
+            Verb = "runas",
+            UseShellExecute = true
+        }
+    };
+    process.Start();
+    process.WaitForExit();
+}
+else
+{
+    Console.WriteLine("Unable to locate GShade uninstaller. Uninstall manually, then hit Enter to continue.");
+    Console.ReadLine();
+}
+
+//now that GShade has uninstalled, recopy our shaders
+var reinstallFolder = Path.Combine(installPath, "gshade-shaders");
+Directory.CreateDirectory(reinstallFolder);
+Directory.CreateDirectory(Path.Combine(reinstallFolder, "Shaders"));
+foreach (var file in Directory.EnumerateFiles(Path.Combine(newShadersFolder, "Shaders")))
+{
+    File.Copy(file, Path.Combine(reinstallFolder, "Shaders", Path.GetFileName(file)));
+}
+Directory.CreateDirectory(Path.Combine(reinstallFolder, "ComputeShaders"));
+foreach (var file in Directory.EnumerateFiles(Path.Combine(newShadersFolder, "ComputeShaders")))
+{
+    File.Copy(file, Path.Combine(reinstallFolder, "ComputeShaders", Path.GetFileName(file)));
+}
+Directory.CreateDirectory(Path.Combine(reinstallFolder, "Textures"));
+foreach (var file in Directory.EnumerateFiles(Path.Combine(newShadersFolder, "Textures")))
+{
+    File.Copy(file, Path.Combine(reinstallFolder, "Textures", Path.GetFileName(file)));
+}
+Directory.Delete(newShadersFolder, true);
+Directory.Move(Path.Combine(installPath, "gshade-presets-backup"), Path.Combine(installPath, "gshade-presets"));
 
 //prompt user to install ReShade
 //TODO: do this ourselves later lol
